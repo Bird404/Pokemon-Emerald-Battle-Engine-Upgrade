aec879e7d25a39d3c7ba00cf52060a66d53f3d78
DizzyEggg <jajkodizzy@wp.pl>
2016-05-01 22:00:26 +0200

    Update pokes.h

--- src/defines/pokes.h
+++ src/defines/pokes.h
@@ -2,5 +2,8 @@
 #define POKE_INDICES_H
 
 #define POKE_CHERRIM 0x200
+#define POKE_DIALGA 0x218
+#define POKE_PALKIA 0x219
+#define POKE_GIRATINA 0x21C || 0x21C //for that another form
 
 #endif /* POKE_INDICES_H */
