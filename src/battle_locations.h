d417d7e133250aeeed7961634c4bd91b3f68e384
DizzyEggg <jajkodizzy@wp.pl>
2016-05-01 22:28:25 +0200

    Update battle_locations.h

--- src/battle_locations.h
+++ src/battle_locations.h
@@ -20,9 +20,13 @@ u8 battle_text_buff1[0x10];
 u8 battle_text_buff2[0x10];
 u8 active_bank;
 void* battlescripts_curr_instruction;
+void* move_anim_cursor;
 u8 multihit_counter;
 u16 chosen_move_by_banks[4];
 u16 dynamic_base_power;
+u8 animation_bank_attacker;
+u8 animation_bank_target;
+u8 turn_order;
 
 u32 poison_status_flag;
 u32 sleep_status_flag;
