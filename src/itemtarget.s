d291d7379b2ce27e41658f5417fa5fafd919aeda
DizzyEggg <jajkodizzy@wp.pl>
2016-06-09 18:55:30 +0200

    Add files via upload

---
+++ src/itemtarget.s
@@ -0,0 +1,20 @@
+.text
+.thumb
+.thumb_func
+.align 2
+
+itemtarget:
+	ldr r0, =(bank_target)
+	ldrb r1, [r0]
+	str r0, [sp]
+	mov r0, #4
+	mov r2, #0
+	mov r3, #0
+	bl ability_battle_effects
+	ldr r0, =(bank_target)
+	ldrb r1, [r0]
+	mov r0, #2
+	mov r2, #0
+	bl item_battle_effects
+	ldr r1, =(0x804C0B3)
+	bx r1
