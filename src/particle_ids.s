110369ca22a0f2b1766d3fca4d7cde9f670100fd
mrdollsteak <bayerfernando@hotmail.com>
2016-07-29 15:44:47 +1000

    Add files via upload
    Adding MrDS's animation changes

---
+++ src/particle_ids.s
@@ -0,0 +1,11 @@
+.text
+.thumb
+.align 2
+
+.equ HAMMER_ARM_particle_ID, 0x31
+.equ BRINE_particle_ID, 0x32
+.equ ACUPRESSURE_particle_ID, 0x33
+.equ ASSURANCE_particle_ID, 0x34
+.equ POWER_TRICK_particle_ID, 0x35
+.equ POISON_JAB_particle_ID, 0x36
+.equ AURA_SPHERE_particle_ID, 0x37
