e94aae163ddbb7ba433a41956cfcdd58f2d3d647
mrdollsteak <bayerfernando@hotmail.com>
2016-08-02 10:54:33 +1000

    moved particle_ids into the particles folder

---
+++ src/anim_particle/particle_ids.s
@@ -0,0 +1,17 @@
+.text
+.thumb
+.align 2
+
+.equ MEGA_EVOLUTION_RAINBOW_particle_ID, 0x31
+.equ MEGA_EVOLUTION_STONE_particle_ID, 0x32
+.equ MEGA_EVOLUTION_SYMBOL_particle_ID, 0x33
+.equ HAMMER_ARM_particle_ID, 0x34
+.equ BRINE_particle_ID, 0x35
+.equ ACUPRESSURE_particle_ID, 0x36
+.equ ASSURANCE_particle_ID, 0x37
+.equ POWER_TRICK_particle_ID, 0x38
+.equ POISON_JAB_particle_ID, 0x39
+.equ AURA_SPHERE_particle_ID, 0x3A
+.equ DRAGON_PULSE_particle_ID, 0x3B
+.equ POWER_GEM_particle_ID, 0x3C
+.equ ENERGY_BALL_particle_ID, 0x3D
