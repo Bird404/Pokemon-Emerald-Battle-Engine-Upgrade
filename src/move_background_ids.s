f1f57c5683f71f9e8efb1b1e3bb643563aaaa131
mrdollsteak <bayerfernando@hotmail.com>
2016-08-02 10:53:48 +1000

    Moved move_background_ids into the background folder

--- src/move_background_ids.s
+++
@@ -1,8 +0,0 @@
-.text
-.thumb
-.align 2
-
-.equ AURA_SPHERE_background_user, 0x1B
-.equ AURA_SPHERE_background_foe, 0x1C
-.equ FOCUS_BLAST_background_user, 0x1D
-.equ FOCUS_BLAST_background_foe, 0x1E
