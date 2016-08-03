710d065f83bbd69ce690a73bb4a453c8fa4b6064
mrdollsteak <bayerfernando@hotmail.com>
2016-08-03 21:23:35 +1000

    Added giga impact

--- src/anim_bg/move_background_ids.s
+++ src/anim_bg/move_background_ids.s
@@ -6,3 +6,6 @@
 .equ AURA_SPHERE_background_foe, 0x1C
 .equ FOCUS_BLAST_background_user, 0x1D
 .equ FOCUS_BLAST_background_foe, 0x1E
+.equ GIGA_IMPACT_background_user, 0x1F
+.equ GIGA_IMPACT_background_foe, 0x20
+.equ GIGA_IMPACT_background_foe_2, 0x21
