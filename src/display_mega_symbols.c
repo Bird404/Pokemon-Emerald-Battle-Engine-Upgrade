cf9dc3840810c9422d375418e25792e559a2e3ed
KDSKardabox <bollygame94@gmail.com>
2016-08-15 22:46:51 +0530

    added healthbar shaker adjustment

--- src/display_mega_symbols.c
+++ src/display_mega_symbols.c
@@ -3,7 +3,7 @@
 #include "vanilla_functions.h"
 #include "types.h"
 #include "new_battle_struct.h"
-
+#include "defines.h"
 #include "megaimages/indicators.h"
 #include "megaimages/mega_trigger.h"
 
@@ -33,33 +33,6 @@ struct template template_trigger = {0x2345, 0x2345, &mega_trigger, (struct frame
 
 
 u8 is_in_triggering_state(u8 bank);
-#define BANK_TO_ATTACH_TRIGGER 0
-
-#define ANIM_STATE 1
-#define HIDE 0
-#define POS_BEHIND_HPBAR 1
-#define SLIDE_OUT 2
-#define SLIDE_IN 3
-#define DISABLE 4
-#define SLIDED_OUT 5
-#define HIDDEN 6
-#define DISABLED 7
-
-#define PALLET_STATE 2
-#define REGULAR 0
-#define LIGHT_UP_TRIGGER 1
-#define TRIGGER_ON 2
-#define REVERT_COLORS 3
-
-#define RELATIVE_X 3
-#define BASE_X 4
-#define SINGLES_HEALTHBOX_X 120
-#define DBL_HB_0_X 120
-#define DBL_HB_2_X 132
-#define SINGLES_HEALTHBOX_Y 90
-#define DBL_HB_0_Y 75
-#define DBL_HB_2_Y 100
-#define TRIGGER_NUM_IGNORED_COLORS 4
 
 u8 is_multi_battle();
 
@@ -109,6 +82,7 @@ void healthbar_trigger_callback(struct object *self)
         if(!(new_battlestruct.ptr->mega_related.evo_happened_pbs&0x5) ||
            (is_multi_battle() && !(new_battlestruct.ptr->mega_related.evo_happened_pbs&0x1)))
             self->private[ANIM_STATE]=HIDDEN;
+
         break;
 
     case POS_BEHIND_HPBAR:
@@ -245,29 +219,50 @@ void hide_trigger_on_pressing_A()
         objects[trigger_id].private[ANIM_STATE]=SLIDE_IN;
 }
 
+
+
 u8 hide_trigger()
 {
     u8 trigger_id=new_battlestruct.ptr->mega_related.trigger_id;
     u8 can_b_button_work=0;
     if(objects[trigger_id].private[ANIM_STATE]==SLIDED_OUT)
+    {
         objects[trigger_id].private[ANIM_STATE]=SLIDE_IN;
-    if(objects[trigger_id].private[ANIM_STATE]==HIDDEN || objects[trigger_id].private[RELATIVE_X]==0)
+    }
+    if(objects[trigger_id].private[ANIM_STATE]==DISABLED ||
+       objects[trigger_id].private[ANIM_STATE]==HIDDEN || objects[trigger_id].private[RELATIVE_X]==0)
+    {
         can_b_button_work=1;
+    }
     return can_b_button_work;
 }
 
-#define DISABLED_INDICATOR 1
-#define UNHIDE_INDICATOR 2
+void unhide_indicators()
+{
+    struct mega_related* mega = &new_battlestruct.ptr->mega_related;
+    for(u8 i = 0; i<no_of_all_banks ;i++)
+    {
+        objects[mega->indicator_id_pbs[i]].private[UNHIDE_INDICATOR]=1;
+    }
+}
+
 void healthbar_indicator_callback(struct object *self)
 {
     if(!self->private[DISABLED_INDICATOR])
     {
         u8 bank=self->private[0];
         struct object healthbox = objects[healthbox_obj_id_pbs[bank]];
+
         u16 slide_dist=healthbox.pos2.x;
         if(slide_dist!=0)
         {
             self->private[UNHIDE_INDICATOR]=1;
+        }
+
+        if(!self->private[FIRED_UNHIDE] && self->pos2.y!=0)
+        {
+            unhide_indicators();
+            self->private[FIRED_UNHIDE]=1;
         }
         if(!self->private[UNHIDE_INDICATOR] || ((bank&1) && !(new_battlestruct.ptr->mega_related.ai_party_mega_check&bits_table[battle_team_id_by_side[bank]]))
            || (!(bank&1) && !(new_battlestruct.ptr->mega_related.party_mega_check&bits_table[battle_team_id_by_side[bank]])))
@@ -291,22 +286,20 @@ void healthbar_indicator_callback(struct object *self)
             self->pos1.x=healthbox.pos1.x+44;
         }
         self->pos2.x=slide_dist;
-
         self->pos1.y=healthbox.pos1.y-5;
-
         // Mirror healthbox priority
         u8 priority = ((healthbox.final_oam.attr2 >> 10) & 3);
         self->final_oam.attr2 = (self->final_oam.attr2 & ~0xC00) | (priority << 10);
     }
 }
 
-void dp11_objects_pingpong(struct object *param_object)
+void dp11_objects_pingpong(struct object *parent_object)
 {
-    u8 some_other_object_id=param_object->private[3];
-    s16 pong=get_pingpong((s16)param_object->private[0],(s16)param_object->private[2]);
-    u16 ychange=param_object->private[2]+pong;
-    objects[some_other_object_id].pos2.y=ychange;
-    if(some_other_object_id==healthbox_obj_id_pbs[0])
+    u8 child_object_id=parent_object->private[3];
+    s16 pong=get_pingpong((s16)parent_object->private[0],(s16)parent_object->private[2]);
+    u16 ychange=parent_object->private[2]+pong;
+    objects[child_object_id].pos2.y=ychange;
+    if(child_object_id==healthbox_obj_id_pbs[0])
     {
         u8 trigger_id=new_battlestruct.ptr->mega_related.trigger_id;
         u8 indi_id=new_battlestruct.ptr->mega_related.indicator_id_pbs[0];
@@ -319,7 +312,7 @@ void dp11_objects_pingpong(struct object *param_object)
             objects[indi_id].pos2.y=ychange;
         }
     }
-    else if(some_other_object_id==healthbox_obj_id_pbs[2])
+    else if(child_object_id==healthbox_obj_id_pbs[2])
     {
         u8 trigger_id=new_battlestruct.ptr->mega_related.trigger_id;
         u8 indi_id=new_battlestruct.ptr->mega_related.indicator_id_pbs[2];
@@ -332,7 +325,89 @@ void dp11_objects_pingpong(struct object *param_object)
             objects[indi_id].pos2.y=ychange;
         }
     }
-    param_object->private[0]=(param_object->private[0]+param_object->private[1])&0xFF;
+    else if(child_object_id==healthbox_obj_id_pbs[1])
+    {
+        u8 trigger_id=new_battlestruct.ptr->mega_related.trigger_id;
+        u8 indi_id=new_battlestruct.ptr->mega_related.indicator_id_pbs[1];
+        if(objects[trigger_id].private[ANIM_STATE]!=DISABLED && objects[trigger_id].private[BANK_TO_ATTACH_TRIGGER]==1)
+        {
+            objects[trigger_id].pos2.y=ychange;
+        }
+        if(!objects[indi_id].private[DISABLED_INDICATOR])
+        {
+            objects[indi_id].pos2.y=ychange;
+        }
+    }
+    else if(child_object_id==healthbox_obj_id_pbs[3])
+    {
+        u8 trigger_id=new_battlestruct.ptr->mega_related.trigger_id;
+        u8 indi_id=new_battlestruct.ptr->mega_related.indicator_id_pbs[3];
+        if(objects[trigger_id].private[ANIM_STATE]!=DISABLED && objects[trigger_id].private[BANK_TO_ATTACH_TRIGGER]==3)
+        {
+            objects[trigger_id].pos2.y=ychange;
+        }
+        if(!objects[indi_id].private[DISABLED_INDICATOR])
+        {
+            objects[indi_id].pos2.y=ychange;
+        }
+    }
+    parent_object->private[0]=(parent_object->private[0]+parent_object->private[1])&0xFF;
+}
+
+void obj_delete_and_free_tiles(struct object *parent_object);
+
+void healthbar_shake(struct object *parent_object)
+{
+    u8 child_object_id=parent_object->private[1];
+    u16 ychange = parent_object->private[0];
+    objects[child_object_id].pos2.y=ychange;
+    parent_object->private[0] = -parent_object->private[0];
+    u16 *reps = &parent_object->private[2];
+    bool is_hbar=true;
+    struct mega_related* mega = &new_battlestruct.ptr->mega_related;
+    struct object *indi_obj;
+
+    if (child_object_id==healthbox_obj_id_pbs[0])
+    {
+        indi_obj=&objects[mega->indicator_id_pbs[0]];
+    }
+    else if (child_object_id==healthbox_obj_id_pbs[1])
+    {
+        indi_obj=&objects[mega->indicator_id_pbs[1]];
+    }
+    else if (child_object_id==healthbox_obj_id_pbs[2])
+    {
+        indi_obj=&objects[mega->indicator_id_pbs[2]];
+    }
+    else if (child_object_id==healthbox_obj_id_pbs[3])
+    {
+        indi_obj=&objects[mega->indicator_id_pbs[3]];
+    }
+    else
+    {
+        is_hbar=false;
+    }
+
+    if(is_hbar)
+    {
+        is_hbar=(is_hbar)&&(!indi_obj->private[DISABLED_INDICATOR])&&(indi_obj->private[UNHIDE_INDICATOR]);
+    }
+    if(is_hbar)
+    {
+        indi_obj->pos2.y=ychange;
+    }
+    (*reps)++;
+    if(*reps==0x15)
+    {
+        if(is_hbar)
+        {
+            indi_obj->pos2.x=0;
+            indi_obj->pos2.y=0;
+        }
+        objects[child_object_id].pos2.x=0;
+        objects[child_object_id].pos2.y=0;
+        obj_delete_and_free_tiles(parent_object);
+    }
 }
 
 void gpu_pal_obj_alloc_tag_and_apply(void *);
